00f54c66c928201aab89262e2cb3f82c1f353eb1
Shifteon <bpwyatt04@gmail.com>
2021-08-24 16:41:22 -0600

    Added a timer to create new penguins (3 seconds)

--- World.gd
+++ World.gd
@@ -8,3 +8,12 @@ var score = 0
 func _on_Dishes_did_dishes():
 	score += 100
 	scoreCounter.text = "Score: %s" % score
+
+func new_penguin():
+	print_debug("New Penguin!!!")
+	var penguin = load("res://penguin.tscn").instance()
+	get_node("YSort").add_child(penguin)
+
+
+func _on_NewPenguinTimer_timeout():
+	new_penguin()
